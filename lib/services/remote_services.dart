11b83342a1b7d27b6e82bf6956f0a17fed3e7204
AlMamun-CSE <almamuncse2020@gmail.com>
2021-09-08 02:33:22 +0600

    Completed Services

---
+++ lib/services/remote_services.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+import 'package:rest_api_and_getx/models/Product.dart';
+
+class RemoteServices {
+  static var client = http.Client();
+
+  static Future<List<Product>> fetchProducts() async {
+    var response = await client.get(Uri.parse(
+        'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'));
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      return productFromJson(jsonString);
+    } else {
+      //show error message
+      return null;
+    }
+  }
+}
