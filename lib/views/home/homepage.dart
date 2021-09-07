5f167f879c8f3805087f5afe7ac9458e6c748d7f
AlMamun-CSE <almamuncse2020@gmail.com>
2021-09-08 02:34:07 +0600

    Complete View

---
+++ lib/views/home/homepage.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:get/get.dart';
+import 'package:rest_api_and_getx/controllers/product_controller.dart';
+import 'package:rest_api_and_getx/views/home/widgets/product_tile.dart';
+class HomePage extends StatelessWidget {
+  final ProductController productController = Get.put(ProductController());
+  //static load data for grid view
+  
+  // final List<String> images = [
+  //   "https://uae.microless.com/cdn/no_image.jpg",
+  //   "https://images-na.ssl-images-amazon.com/images/I/81aF3Ob-2KL._UX679_.jpg",
+  //   "https://www.boostmobile.com/content/dam/boostmobile/en/products/phones/apple/iphone-7/silver/device-front.png.transform/pdpCarousel/image.jpg",
+  //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgUgs8_kmuhScsx-J01d8fA1mhlCR5-1jyvMYxqCB8h3LCqcgl9Q",
+  //   "https://ae01.alicdn.com/kf/HTB11tA5aiAKL1JjSZFoq6ygCFXaw/Unlocked-Samsung-GALAXY-S2-I9100-Mobile-Phone-Android-Wi-Fi-GPS-8-0MP-camera-Core-4.jpg_640x640.jpg",
+  //   "https://media.ed.edmunds-media.com/gmc/sierra-3500hd/2018/td/2018_gmc_sierra-3500hd_f34_td_411183_1600.jpg",
+  //   "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/images/16q1/665019/2016-chevrolet-silverado-2500hd-high-country-diesel-test-review-car-and-driver-photo-665520-s-original.jpg", 
+  //   "https://media.onthemarket.com/properties/6191869/797156548/composite.jpg",
+  //   "https://media.onthemarket.com/properties/6191840/797152761/composite.jpg",
+  // ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: Icon(
+          Icons.arrow_back_ios,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'ShopX',
+                    style: TextStyle(
+                        fontFamily: 'avenir',
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ),
+                IconButton(
+                    icon: Icon(Icons.view_list_rounded), onPressed: () {}),
+                IconButton(icon: Icon(Icons.grid_view), onPressed: () {}),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Obx(() {
+              if (productController.isLoading.value)
+                return Center(child: CircularProgressIndicator());
+              else
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 2,
+                  itemCount: productController.productList.length,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  itemBuilder: (context, index) {
+                    return ProductTile(productController.productList[index]);
+                  },
+                  staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                );
+            }),
+          )
+        ],
+      ),
+    );
+  }
+}
